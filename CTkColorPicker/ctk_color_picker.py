2fa337edd8ddf1e6c0e9ac586de1c5f94677f3c3
UX0l0l <w4v3n@protonmail.com>
2024-05-21 01:50:51 +0300

    Re-added author and contribution banner (removed by accident)

--- CTkColorPicker/ctk_color_picker.py
+++ CTkColorPicker/ctk_color_picker.py
@@ -1,3 +1,7 @@
+# CTk Color Picker for customtkinter
+# Original Author: Akash Bora (Akascape)
+# Contributers: Victor Vimbert-Guerlais (helloHackYnow), UX0l0l
+
 from tkinter import *
 from customtkinter import *
 from PIL import Image, ImageTk
